50cf0e543bfdc3568201f37171f8b7b716f6d8bb
tabassum2507 <10tabassum2@gmail.com>
2022-09-02 12:35:41 +0530

    context api added

---
+++ expensetracker/src/context/GlobalState.js
@@ -0,0 +1,29 @@
+import React, { createContext, useReducer} from "react";
+import AppReducer from './AppReducer';
+
+//initial state
+const initialState = {
+    transaction: [
+        { id: 1, text: 'Flower', amount: -20 },
+        { id: 2, text: 'Salary', amount: 300 },
+        { id: 3, text: 'Book', amount: -10 },
+        { id: 4, text: 'Camera', amount: 150 }
+    ]
+}
+
+//create context
+
+export const GlobalContext = createContext(initialState);
+
+// provider comnponent
+
+export const GlobalProvider = ({children}) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return(<GlobalContext.Provider value={{
+        transaction : state.transaction
+    }}>
+    {childrean}
+    </GlobalContext.Provider>);
+}
+
