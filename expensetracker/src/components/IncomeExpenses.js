66210993c9ca656ac7c7373c84ba941456ae5506
tabassum2507 <10tabassum2@gmail.com>
2022-09-02 11:47:51 +0530

    ui completed

--- expensetracker/src/components/IncomeExpenses.js
+++ expensetracker/src/components/IncomeExpenses.js
@@ -2,15 +2,15 @@ import React from "react";
 
 export default function IncomeExpenses() {
   return (
-    <div  class="inc-exp-container">
+    <div  className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p id="money-plus" className="money plus">+$0.00</p>
+        <p className="money plus">+$0.00</p>
       </div>
 
       <div>
         <h4>Expense</h4>
-        <p id="money-minus" className="money minus">-$0.00</p>
+        <p className="money minus">-$0.00</p>
       </div>
     </div>
   );
